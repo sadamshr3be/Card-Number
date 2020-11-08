3a5b468ce771a85e19e6d42883b9a1e7e6e1d756
sadamalsharabi <44142745+sadamshr3be@users.noreply.github.com>
2020-11-08 23:31:28 +0300

    Add files via upload

---
+++ Card.sh
@@ -0,0 +1,30 @@
+#!/data/data/com.termux
+
+
+تثبيت الاداة 
+
+
+$  apt update && apt upgrade
+
+$  apt install python
+
+$  apt install git
+
+$   git clone https://github.com/sadamshr3be/Card-Number
+
+$  cd Card-Number
+
+$  ls
+
+$  chomd 777 Card.sh
+
+$  sh Card.sh
+نكتب مقابل 
+[#]  Count      : 10
+[#]  Delay       : 2
+
+قناتي علـّۓ. التليجرام
+https://t.me/termuxalsharabi
+
+قناتي علـّۓ. اليوتيوب
+https://www.youtube.com/channel/UC6KWVWMUn210EJYDyUHQhKQ
